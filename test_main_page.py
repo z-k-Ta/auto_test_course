b58d86fde9513484b1467ae0be85b8c44b28983e
Your Name <you@example.com>
2022-05-06 17:25:05 +0300

    add test to login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,27 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+
+def test_login(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
+
