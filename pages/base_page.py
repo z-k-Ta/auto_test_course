68ec52504346bcdefeb5c562f084b5b7874e5250
Your Name <you@example.com>
2022-05-06 17:27:52 +0300

    add NoSuchElementException

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,11 @@
 from selenium.common.exceptions import NoSuchElementException
 
+
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
-
 
     def open(self):
         self.browser.get(self.url)
@@ -16,3 +16,4 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+
