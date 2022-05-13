6a7017d9f934b9d20059ecdcb5faa569b1df180e
Your Name <you@example.com>
2022-05-13 16:12:28 +0300

    create test_product_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,7 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.market_page import MarketPage
+import pytest
 
 
 def test_guest_can_go_to_login_page(browser):
